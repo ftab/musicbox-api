4cb1d79c58908521376651dc078619e23d8b3894
Dennis Field <fury@xibase.com>
2024-08-01 18:14:00 -0400

    That was pretty easy

--- server.js
+++ server.js
@@ -3,10 +3,21 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+const usersRouter = require("./routes/users");
+
 app.get("/", (req, res) => {
   return res.status(200).send({
     message: "Hello World!",
   });
+});
+
+app.use("/users", usersRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  console.error(err.message, err.stack);
+  res.status(statusCode).json({ message: err.message });
+  return;
 });
 
 app.listen(port, () => {
