58c4e53304764152494a75cc9c84f347362feebf
Dennis Field <fury@xibase.com>
2024-08-04 15:50:39 -0400

    Whoops

--- services/videos.js
+++ services/videos.js
@@ -5,10 +5,10 @@ const config = require('../config');
 async function getMultiple(userid, page = 1){
   const offset = helper.getOffset(page, config.listPerPage);
   const rows = await db.query(
-    `SELECT videoId, youtubeId, soundcloudId, vimeoId, bandcampId, isFlagged, tags, title
-    FROM video WHERE videoId IN (
-      SELECT videoId FROM user_video WHERE userId = ?
-    ) ORDER BY uservideoId LIMIT ?,?`,
+    `SELECT uservideoId, videoId, youtubeId, soundcloudId, vimeoId, bandcampId, isFlagged, tags, title
+    FROM video LEFT JOIN user_video USING (videoId)
+    WHERE userId = ?
+    ORDER BY uservideoId LIMIT ?,?`,
     [userid, offset, config.listPerPage]
   );
   const data = helper.emptyOrRows(rows);
